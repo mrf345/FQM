9153aeba0735a53b1e6ac8f633d195a41a7090d2
mrf345 <mrf345@gmail.com>
2018-12-19 08:55:07 +0000

    updating test window printer batch script

--- test_printer_windows.bat
+++ test_printer_windows.bat
@@ -1,24 +1,18 @@
 @echo off
 rem Script to test printing on windows
-rem Latest update 12/17/2018
 
 
 :head
 setlocal enabledelayedexpansion
-    echo Setting up temporary files
-    set /a limit=3
+    rem setting up temporary files
+    set /a limit=5
 :temping
     rem recursing to create empty temp files
     set /a counter=counter+1
     if !counter! gtr !limit! ( goto :body )
     echo. > temp!counter!
-goto :body
+goto :temping
 
-:run
-rem to run vbscript
-rem %1 vbscript file
-CSCRIPT.EXE %1
-goto :eof
 
 :notify
 rem to show message box
@@ -29,38 +23,74 @@ if %1 equ 1 (
     echo msgbox%2,48,%3 > temp.vbs
     temp.vbs
 )
-del /q /a temp.vbs
 goto :eof
+
 
 :input
 rem to get user input using vbs input box
 rem parameters: %1 main %2 title %3 placeholder
-echo inputbox %1, %2, %3 > temp.vbs
-for /f "tokens=*" %%return in ('temp.vbs') do (
-  set PRINTER='%%return'
-)
-del /q /a temp.vbs
+echo inputed=inputbox(%1, %2, %3) > temp.vbs
+echo Set objFSO=CreateObject("Scripting.FileSystemObject") >> temp.vbs
+echo Set objFile = objFSO.CreateTextFile("temp1",True) >> temp.vbs
+echo objFile.Write inputed ^& vbCrLf >> temp.vbs
+echo objFile.Close >> temp.vbs
+temp.vbs
+set /p INPUTED=<temp1
 goto :eof
+
+
+set /a second_counter=0
+:spacer
+rem %1 number of lines, %2 file name
+if !second_counter! gtr %1 ( 
+	set /a second_counter=0
+	goto :eof
+)
+echo. >> %2
+set /a second_counter=second_counter+1
+goto :spacer
 
 
 :body
 rem getting the printer and test printing
-setlocal
-call :input "Enter shared printer name:" "Testing printer" "printer name"
-echo !PRINTER!
+call :input "Enter shared printer name:" "Testing printer" ""
+rem lets check if inputed is in the list of printers
+wmic printer get name | find "!INPUTED!" && (
+    call :notify 1 "The entered printer name was found successfully" "Processing printer"
+    rem lets check if the printer is shared printer with identical name
+    wmic printer get sharename | find "!INPUTED!" && (
+        call :notify 1 "The entered printer's shared with the correct name" "Processing printer"
+        call :notify 1 "Now will attempt printing, fingers across" "Printing"
+        rem attempting to print a template ticket
+		call :spacer 6 temp4
+		echo FQM is printing, Cheers... >> temp4
+		call :spacer 6 temp4
+        print /D:\\localhost\"!INPUTED!" temp4 > temp5
+		set /p response=<temp5 
+		call :notify 1 "!response!" "Printing test result"
+		goto :end
+    ) || (
+        call :notify 2 "The entered printer either not shared on the local network or the share name is not identical to the printer's original name" "Failed to print"
+        call :notify 2 "Make sure to enable sharing the printer on the local network and copy the original name of the printer to be the share name. Then try this again" "Failed to print"
+        goto :end
+    )
+) || (
+    call :notify 2 "The entered printer name was not found in the connected printers list" "Processing printer"
+    call :notify 2 "Make sure of your printers USB connection and try this again" "Failed to print"
+    goto :end
+)
 goto :end
 
 
 :end
 rem clean up and delete temp files
-call :notify 1 "All done, press ok to delete couple temprory files and exit ." "Clean up and exiting"
-:clean
-    rem recursing to remove temp files
-    del /q /a temp!counter!
-    set /a counter=!counter!-1
-    if !counter! equ 0 (
-        goto :eof
-    )
-goto :clean
-set /p msg=Press enter to exit ...
-exit 
+rem recursing to remove temp files
+del /q /a temp!counter!
+set /a counter=counter-1
+if !counter! equ 0 (
+    call :notify 1 "Cleanup is done. Press ok to exit ." "Clean up and exiting"
+    del /q /a temp.vbs
+	goto :eof
+)
+goto :end
+exit
