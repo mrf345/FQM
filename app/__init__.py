de9e8ab1a91e2a0e69971f9c23377f97e717b048
Mohamed Feddad <mrf345@gmail.com>
2020-09-03 13:32:22 +0300

    Add additional application for gunicorn.

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,9 @@
+import os
 from app.main import bundle_app # noqa
 
 # NOTE: uncomment out while genrating migration
 # app = bundle_app({'MIGRATION': True})
+
+application = bundle_app({
+    'CLI_OR_DEPLOY': True,
+    'GUNICORN': 'gunicorn' in os.environ.get('SERVER_SOFTWARE', '')})  # noqa
