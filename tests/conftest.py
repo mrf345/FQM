0a21f7a9014c0d001a220d5f7e210ea551acf950
Mohamed Feddad <mrf345@gmail.com>
2020-09-06 20:01:07 +0300

    Add await_task fixture.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,2 +1,19 @@
-import pytest  # noqa
+import pytest
+from time import sleep
+
 from . import c  # noqa
+from app.tasks import stop_tasks
+
+@pytest.fixture
+def await_task():
+    def waitier(task):
+        await_task.__dict__['TASK'] = task
+
+        while not task.spinned_once:
+            sleep(1)
+
+    yield waitier
+
+    stop_tasks()
+    while not await_task.__dict__['TASK'].dead:
+        sleep(1)
