11be3f41b0d3d8e7244bb3c89a009b5e8d48e128
Mohamed Feddad <mrf345@gmail.com>
2020-09-10 16:03:23 +0300

    Refactor and cleanup bg tasks tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,18 +2,24 @@ import pytest
 from time import sleep
 
 from . import c  # noqa
-from app.tasks import stop_tasks
+from app.tasks import stop_tasks, get_task
 
 @pytest.fixture
-def await_task():
-    def waitier(task):
-        await_task.__dict__['TASK'] = task
+def get_bg_task():
+    def waitier(task_name):
+        task = get_task(task_name)
+        get_bg_task.__dict__['TASK'] = task
+
+        if not task:
+            raise AttributeError(f'Background Task `{task_name}` is not running.')
 
         while not task.spinned_once:
             sleep(1)
 
+        return task
+
     yield waitier
 
     stop_tasks()
-    while not await_task.__dict__['TASK'].dead:
+    while not get_bg_task.__dict__['TASK'].dead:
         sleep(1)
