96a3764790cc899775aa6bc7362b8e0e19a46061
Mohamed Feddad <mrf345@gmail.com>
2020-09-12 21:47:55 +0300

    Refactor API setup helper for tests.

--- app/api/setup.py
+++ app/api/setup.py
@@ -2,12 +2,19 @@ from flask import Blueprint
 
 from app.api import api
 from app.api.endpoints.tickets import setup_tickets_endpoint
+from app.api.endpoints.tasks import setup_tasks_endpoint
 
 
 def setup_api():
-    blueprint = Blueprint('api', __name__)
+    blueprint = setup_api.__dict__.get('blueprint')
+
+    if blueprint:
+        return blueprint
+
+    blueprint = setup_api.__dict__['blueprint'] = Blueprint('api', __name__)
 
     api.init_app(blueprint)
     setup_tickets_endpoint()
+    setup_tasks_endpoint()
 
     return blueprint
