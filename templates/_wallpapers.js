300dcf2afb869fa6dd69a38c874bad3736cb7ae5
Mohamed Feddad <mrf345@gmail.com>
2024-03-16 01:48:04 +0400

    fix: disable online wallpapers script
    Resolves #295

--- templates/_wallpapers.js
+++ templates/_wallpapers.js
@@ -1,18 +1,5 @@
-{% macro wallpapers(selector, enable_reddit=True, overlay="rgba(0,0,0,0.7)", duration=60) %}
+{% macro wallpapers(selector, enable_reddit=False, overlay="rgba(0,0,0,0.7)", duration=60) %}
 // Macro to set the background wallpapers for a given element via `selector`
-{% if enable_reddit %}
-RedditWallpapers({
-    isFixed: 'true',
-    id: '{{ selector }}',
-    isOverlayed: 'true',
-    overlay: '{{ overlay }}',
-    duration: {{ duration }},
-    limit: 20,
-    timeout: 1,
-    category: ['CityPorn'],
-    defaultImg: "{{ url_for('static', filename='images/dbg.jpg') }}"
-})
-{% else %}
 $('{{ selector }}').css({
     'background-size': 'cover',
     'background-repeat': 'no-repeat',
@@ -20,5 +7,4 @@ $('{{ selector }}').css({
     'background-attachment': 'fixed',
     'background-image': "linear-gradient({{ overlay }}, {{ overlay }}), url({{ url_for('static', filename='images/dbg.jpg') }})"
 })
-{% endif %}
 {% endmacro %}
